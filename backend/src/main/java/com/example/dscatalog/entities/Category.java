64628ff142dfe6393ba224b934eb4d58655dea0f
Jonatas Melo <jonnmello@gmail.com>
2024-02-02 10:56:17 -0300

    Auditing data

--- backend/src/main/java/com/example/dscatalog/entities/Category.java
+++ backend/src/main/java/com/example/dscatalog/entities/Category.java
@@ -1,11 +1,15 @@
 package com.example.dscatalog.entities;
 
+import java.time.Instant;
 import java.util.Objects;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.PrePersist;
+import jakarta.persistence.PreUpdate;
 import jakarta.persistence.Table;
 
 @Entity
@@ -16,6 +20,12 @@ public class Category {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String name;
+	
+	@Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
+	private Instant createdAt;
+	
+	@Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
+	private Instant updateAt;
 	
 	public Category() {
 		
@@ -42,6 +52,27 @@ public class Category {
 		this.name = name;
 	}
 
+	
+	
+	public Instant getCreatedAt() {
+		return createdAt;
+	}
+
+
+	public Instant getUpdateAt() {
+		return updateAt;
+	}
+
+	@PrePersist
+	public void prePersist() {
+		createdAt = Instant.now();
+	}
+	
+	@PreUpdate
+	public void preUpdate() {
+		updateAt = Instant.now();
+	}
+	
 	@Override
 	public int hashCode() {
 		return Objects.hash(id);
